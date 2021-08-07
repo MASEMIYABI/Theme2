e8dca7827cff2cf653f63627ccb62c8a81694b5d
Masemiyabi <miyabi.kanjani@ezweb.ne.jp>
2021-08-07 08:11:01 +0000

    ADD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,16 +28,18 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+ 
 
-  private
-  def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image)
-  end
+    private
+     def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
+     end
 
-  def ensure_correct_user
-    @user = User.find(params[:id])
-    unless @user == current_user
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user
       redirect_to user_path(current_user)
+      end
     end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
    devise_for :users
    root 'homes#top'
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update]do
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    end
+
   resources :books,only: [:new, :create, :index, :show, :destroy, :edit, :update] do
    resource :favorites, only: [:create, :destroy]
   resources :book_comments,only: [:create,:destroy]
   end
   get 'home/about' => 'homes#about'
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
 end
